b8f6ffa34e063186c8e69a8769a429f5a17c0978
rpolicastro <rpolicas@indiana.edu>
2019-08-06 14:56:20 -0400

    finally got normalization working again

--- R/normalize.R
+++ R/normalize.R
@@ -7,35 +7,35 @@
 #' @import tibble
 #' @import dplyr
 #' @import edgeR
-#' @importFrom magrittr %>%
 #' @importFrom tidyr spread
 #' @importFrom purrr map
+#' @importFrom magrittr %>%
 #'
 #' @param experiment tsrexplorer object
 #'
 #' @return tibble of TMM normalized read counts
 #'
 #' @export
-#' @rdname normalize-methods
+#' @rdname normalize-method
 
 setGeneric(
 	"normalize",
-	function(experiment) standardGeneric("normalize"),
+	function(experiment) standardGeneric("normalize")
 )
 
-#' @rdname normalize-methods
+#' @rdname normalize-method
 
 setMethod(
 	"normalize", signature="tsr_object",
 	function(experiment) {
-		tss_granges <- experiment@experiment_info
+		tss_granges <- experiment@experiment
 		
 		counts <- tss_granges %>%
 			# Make the TSS name a concatenation of the chromosome, start, end, and strand.
 			map(
 				~as_tibble(., .name_repair = "unique") %>%
 				mutate(position = paste(seqnames, start, end, strand, sep="_")) %>%
-				dplyr::select(position, score)
+				select(position, score)
 			) %>%
 			# Select the TSS name column and the score.
 			# Turn the list of tibbles into one tibble with a column specify what tibble the row came from.
@@ -59,5 +59,6 @@ setMethod(
 			as_tibble(rownames="TSS_position", .name_repair="unique")
 
 		experiment@TMM <- tmm_tbl
+		return(experiment)
 	}
 )
--- R/tsrexplorer.R
+++ R/tsrexplorer.R
@@ -1,23 +1,13 @@
-#' tsrexplorer object
-#'
-#' @importFrom tibble tibble
-#'
-#' @slot experiment_info List of TSS GRanges returned by TSRchitect
-#' @slot organism Organism name
-#' @slot cores Number of CPU cores/threads available
-#' @slot TMM TMM normalized TSS read counts
 
 setClass(
 	"tsr_object",
 	representation(
-		experiment_info = "list",
-		organism = "character",
+		experiment = "list",
 		cores = "numeric",
 		TMM = "tbl"
 	),
 	prototype(
-		experiment_info = list(),
-		organism = NA_character_,
+		experiment = list(),
 		cores = NA_real_,
 		TMM = tibble()
 	)
@@ -25,19 +15,19 @@ setClass(
 
 #' TSRexplorer constructor function.
 #'
-#' @param experiment_info List of TSS GRanges returned by TSRchitect
-#' @param organism Name of organism associated with data
+#' @param experiment List of TSS GRanges returned by TSRchitect
 #' @param cores Number of CPU cores/threads available
 #'
 #' @return A tsrexplorer object
 #'
-#' @examples tsrexplorer(TSSs, "S. cerevisiae", 4)
+#' @examples tsrexplorer(TSSs, 4)
 #'
 #' @importFrom GenomicRanges GRanges
+#' @importFrom tibble tibble
 #'
 #' @export
 
-tsrexplorer <- function(experiment_info, cores=1) {
+tsr_explorer <- function(experiment, cores=1) {
 		cores <- as.integer(cores)
-		new("tsr_object", experiment_info=experiment_info, cores=cores)
+		new("tsr_object", experiment=experiment, cores=cores)
 }
