5b656f98a8519a4f0ac8ae7b676c36b7634e8a14
rpolicastro <rpolicas@indiana.edu>
2019-11-13 14:37:05 -0500

    updating annotation compatability

--- R/annotate.R
+++ R/annotate.R
@@ -17,7 +17,6 @@
 #' @param feature_type Annotate on gene or transcript level
 #' @param upstream Bases upstream of TSS
 #' @param downstream Bases downstream of TSS
-#' @param normalized Whether to use the normalized (TRUE) or raw (FALSE) counts
 #'
 #' @return Annotated TSS slot in tsrchitect object
 #'
@@ -31,46 +30,47 @@ annotate_features <- function(
 	data_type = c("tss", "tsr"),
 	feature_type = c("gene", "transcript"),
 	upstream = 1000,
-	downstream = 100,
-	normalized = TRUE
+	downstream = 100
 ) {
 	## Load GTF.
 	genome_annotation <- makeTxDbFromGFF(annotation_file, format = "gtf")
 
 	## Grab data from proper slot.
-	if (data_type == "tss" & normalized) {
-		annotated <- experiment@normalized_counts$TSSs
-	} else if (data_type == "tss" & !(normalized)) {
-		annotated <- experiment@raw_counts$TSSs
-	} else if (data_type == "tsr" & normalized) {
-		annotated <- experiment@normalized_counts$TSRs
-	} else {
-		annotated <- experiment@raw_counts$TSRs
+	if (data_type == "tss") {
+		raw <- experiment@counts$TSSs$raw
+		cpm_counts <- experiment@counts$TSSs$cpm
+	} else if (data_type == "tsr") {
+		raw <- experiment@counts$TSRs$raw
+		cpm_counts <- experiment@counts$TSRs$cpm
 	}
 
-	## Prepare data for annotation.
-	annotated <- annotated %>%
-		separate(position, into = c("seqnames", "start", "end", "strand"), sep = "_") %>%
-		gather(-seqnames, -start, -end, -strand, key = "sample", value = "score") %>%
-		filter(score > 0) %>%
-		split(.$sample) %>%
-		map(~ select(.x, -sample) %>% makeGRangesFromDataFrame(keep.extra.columns = TRUE))
-		
 	## Annotate features.
-	annotated <- map(annotated,
-		~ annotatePeak(.,
-			tssRegion = c(-upstream, downstream),
-			TxDb = genome_annotation,
-			sameStrand = TRUE,
-			level = feature_type,
-		) %>% as_tibble(.name_repair="unique")
+	raw_annotated <- raw %>%
+		map(
+			~ annotatePeak(.,
+				tssRegion = c(-upstream, downstream),
+				TxDb = genome_annotation,
+				sameStrand = TRUE,
+				level = feature_type,
+			) %>% as_tibble(.name_repair="unique")
 	)
+
+	cpm_annotated <- cpm_counts %>%
+                map(
+                        ~ annotatePeak(.,
+                                tssRegion = c(-upstream, downstream),
+                                TxDb = genome_annotation,
+                                sameStrand = TRUE,
+                                level = feature_type,
+                        ) %>% as_tibble(.name_repair="unique")
+		)
+
 
 	## Place annotated features back into tsrexplorer object.
 	if (data_type == "tss") {
-		experiment@annotated$TSSs <- annotated
+		experiment@annotated$TSSs <- list("raw" = raw_annotated, "cpm" = cpm_annotated)
 	} else if (data_type == "tsr") {
-		experiment@annotated$TSRs <- annotated
+		experiment@annotated$TSRs <- list("raw" = raw_annotated, "cpm" = cpm_annotated)
 	}
 
 	return(experiment)
