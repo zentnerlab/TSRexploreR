2e826c8847bdb1d8a6f1ead397a4328f027154b2
rpolicastro <rpolicas@indiana.edu>
2019-12-03 12:27:23 -0500

    adding the actual threshold exploration R script

---
+++ R/thresholding.R
@@ -0,0 +1,102 @@
+
+#' Threshold Exploration
+#'
+#' Explore various naive thresholds
+#'
+#' @import tibble
+#' @importFrom GenomicFeatures makeTxDbFromGFF
+#' @importFrom ChIPseeker annotatePeak
+#' @importFrom dplyr group_by ungroup mutate bind_rows filter pull n_distinct
+#' @importFrom purrr map
+#'
+#' @param experiment tsrexplorer object with TSSs
+#' @param annotation_file Either path and file name of annotation, or a TxDb object
+#' @param feature_type Annotate on gene or transcript level
+#' @param samples Either 'all' or the names of the samples to analyze
+#' @param max_threshold Threshold from 1 to max_threshold will be explored
+#' @param upstream Bases upstream of TSS
+#' @param downstream Bases downstream of TSS
+#'
+#' @return tibble containing information for each threshold and sample
+#'
+#' @rdname explore_thresholds-function
+#'
+#' @export
+
+explore_thresholds <- function(
+	experiment,
+	annotation_file,
+	feature_type = c("gene", "transcript"),
+	max_threshold = 50,
+	upstream = 1000,
+	downstream = 100,
+	samples = "all"
+) {
+	## Pull out appropriate samples.
+	if (all(samples == "all")) {
+		samples <- names(experiment@experiment$TSSs)
+	}
+
+	select_samples <- experiment@experiment$TSSs[samples]
+
+	## Prepare annotation file.
+	if (class(annotation_file) == "character") {
+		annotation <- makeTxDbFromGFF(annotation_file)
+	} else if (class(annotation_file) == "TxDb") {
+		annotation <- annotation_file
+	}
+
+	## Annotate TSSs.
+	raw_annotated <- select_samples %>%
+		map(
+			~ annotatePeak(.x,
+				tssRegion = c(-upstream, downstream),
+				TxDb = annotation,
+				sameStrand = TRUE,
+				level = feature_type,		
+			) %>% as_tibble(.name_repair = "unique")
+		)
+
+	## Grab information needed for thresholding plot.
+	summarize_data <- function(dataset, max) {
+		set_stats <- map(1:max, function(x) {
+			filtered_dataset <- filter(dataset, score >= x)
+
+			total_genes <- filtered_dataset %>%
+				pull(geneId) %>%
+				n_distinct
+
+			genes_with_promoter_proximal_tss <- filtered_dataset %>%
+				filter(annotation == "Promoter") %>%
+				pull(geneId) %>%
+				n_distinct
+
+			total_TSSs <- nrow(filtered_dataset)
+
+			promoter_proximal_TSSs <- filtered_dataset %>%
+				filter(annotation == "Promoter") %>%
+				nrow
+
+			frac_promoter_proximal_TSSs <- promoter_proximal_TSSs / total_TSSs
+
+			result <- tibble(
+				"threshold" = x,
+				"total_genes" = total_genes,
+				"genes_with_promoter_proximal_tss" = genes_with_promoter_proximal_tss,
+				"total_TSSs" = total_TSSs,
+				"promoter_proximal_TSSs" = promoter_proximal_TSSs,
+				"frac_promoter_proximal_TSSs" = frac_promoter_proximal_TSSs
+			)
+
+			return(result)
+		}) %>% bind_rows
+		
+		return(set_stats)
+	}
+
+	threshold_data <- raw_annotated %>%
+		map(~ summarize_data(.x, max_threshold)) %>%
+		bind_rows(.id = "sample")
+		
+	return(threshold_data)
+}
