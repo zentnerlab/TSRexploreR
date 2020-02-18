79ef619ea6b511bcb50545baa5ea96c9a040f112
rpolicastro <rpolicas@indiana.edu>
2020-02-18 09:28:42 -0500

    added dimensionality reduction

---
+++ R/dim_reduction.R
@@ -0,0 +1,62 @@
+
+#' Dimensionality Reduction.
+#'
+#' @importFrom uwot umap
+#'
+#' @param experiment tsrexplorer object
+#' @param data_type Either 'tss', 'tsr', 'tss_features', or 'tsr_features'
+#' @param method 'umap' or 'pca'
+#' @param n_neighbors Nearest neighbors parameter for umap
+#' @param min_dist Minimum distance between points parameter for umap
+#' @param ... Additional arguments passed to geom_point
+#'
+#' @rdname plot_reduction-function
+#' @export
+
+plot_reduction <- function(
+	experiment, data_type = c("tss", "tsr", "tss_features", "tsr_features"),
+	method = "umap", n_neighbors = 2, min_dist = 0.1, ...
+) {
+	
+	## Grab TMM counts.
+	if (data_type == "tss") {
+		tmm_counts <- experiment@correlation$TSSs$tmm
+	} else if (data_type == "tsr") {
+		tmm_counts <- experiment@correlation$TSRs$tmm
+	} else if (data_type == "tss_features") {
+		tmm_counts <- experiment@correlation$TSS_features$tmm
+	} else if (data_type == "tsr_features") {
+		tmm_counts <- experiment@correlation$TSR_features$tmm
+	}
+
+	## Filter out counts with too little expression.
+	keep_index <- tmm_counts %>%
+		assay("filtered") %>%
+		filterByExpr
+
+	tmm_counts <- tmm_counts[keep_index, ]
+
+	## Prepare data for umap.
+	tmm_counts <- tmm_counts %>%
+		assay("tmm") %>%
+		t %>%
+		as.data.frame %>%
+		rownames_to_column("sample") %>%
+		as.data.table
+
+	## UMAP dimensionality reduction.
+	umap_results <- tmm_counts %>%
+		umap(n_neighbors = n_neighbors, min_dist = min_dist)
+	
+	umap_results <- as.data.table(umap_results)
+	setnames(umap_results, old = c("V1", "V2"), new = c("UMAP_1", "UMAP_2"))
+	umap_results[, sample := tmm_counts[["sample"]]]
+
+	## Plot UMAP.
+	p <- ggplot(umap_results, aes(x = UMAP_1, y = UMAP_2, color = sample)) +
+		geom_point(...) +
+		theme_bw() +
+		scale_color_viridis_d()
+
+	return(p)
+}
