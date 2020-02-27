8367acd8e39e023b69d3ec27ad2eed192dd606d1
rpolicastro <rpolicas@indiana.edu>
2020-02-27 16:02:06 -0500

    finishing tsr metrics function

--- R/tsr_metrics.R
+++ R/tsr_metrics.R
@@ -33,7 +33,53 @@ tsr_metrics <- function(experiment) {
 	setkeyv(pb, keys)
 	select_samples <- merge(select_samples, pb, all.x = TRUE)
 
+	## Calculate IQR.
+	iqr <- iq_range(select_samples)
+	setkeyv(iqr, keys)
+	select_samples <- merge(select_samples, iqr, all.x = TRUE)
+
+	## Add metrics back to TSRs.
+	tsr_names <- select_samples[["tsr_sample"]] %>%
+		unique %>%
+		discard(~ is.na(.))
+	
+	select_TSRs <- experiment %>%
+		extract_counts("tsr", tsr_names) %>%
+		bind_rows(.id = "tsr_sample")
+	setnames(select_TSRs, old = "FID", new = "TSR_FID")
+
+	tsr_metrics <- select_samples[
+		!is.na(TSR_FID),
+		.(tsr_sample, TSR_FID, shape_index, shape_class, peak_concentration,
+		peak_balance, iqr_min, iqr_max, iqr_width, iqr_coords)
+	]
+	tsr_metrics <- unique(tsr_metrics)	
+
+	tsr_keys <- c("tsr_sample", "TSR_FID")
+	setkeyv(select_TSRs, tsr_keys)
+	setkeyv(tsr_metrics, tsr_keys)
+
+	select_TSRs <- merge(select_TSRs, tsr_metrics, all.x = TRUE)
+
 	## Add metrics back to tsrexplorer object.
+	select_TSRs <- split(select_TSRs, select_TSRs$tsr_sample)
+	walk(select_TSRs, function(x) {
+		x[, tsr_sample := NULL]
+		setnames(x, old = "TSR_FID", new = "FID")
+		x <- makeGRangesFromDataFrame(x, keep.extra.columns = TRUE)
+		x <- as.data.table(x)
+		return(x)
+	})
+
+	select_samples <- split(select_samples, select_samples$sample)
+	walk(select_samples, function(x) {
+		x[, sample := NULL]
+		x <- makeGRangesFromDataFrame(x, keep.extra.columns = TRUE)
+		x <- as.data.table(x)
+		return(x)
+	})
+
+	experiment@counts$TSRs$raw <- select_TSRs
 	experiment@counts$TSSs$raw <- select_samples
 	return(experiment)
 }
@@ -118,4 +164,68 @@ peak_balance <- function(tss_table) {
 	## Return peak balance values.
 	return(copy(pb_results))
 
+}
+
+#' Interquartile Range
+#'
+#' Find the IQR for a TSR.
+#'
+#' @param tss_table data.table of TSSs
+#'
+#' @rdname iq_range-function
+#' @export
+
+iq_range <- function(tss_table) {
+	
+	## Get TSS Position relative to TSR midpoints.
+        tss_position <- tss_table[
+                !is.na(TSR_FID) & score > 1,
+                .(score, seqnames, start, strand, tss_pos = ifelse(
+                	strand == "+",
+                        start - median(range(start)),
+                        median(range(start)) - start
+                )),
+                by = .(sample, TSR_FID)
+        ]
+
+	## Find the IRQ.
+	tss_position <- tss_position[order(sample, TSR_FID, tss_pos)]
+
+	tss_position[,
+		cum_sum := cumsum(score),
+		by = .(sample, TSR_FID)
+	][,
+		ecdf := ecdf_function(cum_sum),
+		by = .(sample, TSR_FID)
+	]
+
+	iqr_results <- tss_position[
+		dplyr::between(ecdf, 0.1, 0.9)
+	][,
+		.(iqr_min = min(cum_sum),
+		iqr_max = max(cum_sum),
+		iqr_width = max(tss_pos) - min(tss_pos),
+		iqr_coords = str_c(seqnames, min(start), max(start), strand, sep = ":")),
+		by = .(sample, TSR_FID)
+	]
+	iqr_results <- unique(iqr_results)
+
+	## Return the IQR results.
+	return(copy(iqr_results))
+
+}
+
+#' ECDF Function
+#'
+#' Return ECDF values
+#'
+#' @param tss_vector Vector of values to compute ECDF values
+#'
+#' @rdname ecdf_function-function
+#' @export
+
+ecdf_function <- function(tss_vector) {
+	ecdf_func <- ecdf(tss_vector)
+	ecdf_values <- ecdf_func(tss_vector)
+	return(ecdf_values)
 }
