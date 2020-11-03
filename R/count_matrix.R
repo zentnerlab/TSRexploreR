d9f7fc0ee3afa721b962594c09415facb3bb1ed9
rpolicastro <rpolicas@indiana.edu>
2020-11-03 12:20:53 -0500

    adding merged TSR support for count matrix generation

--- R/count_matrix.R
+++ R/count_matrix.R
@@ -2,7 +2,6 @@
 #' Generate Count Matrix.
 #'
 #' @param count_data Names list of counts.
-#' @param data_type Currently supports 'tss' only.
 #' @param use_normalized Whether to use normalized counts.
 
 .count_matrix <- function(
@@ -13,12 +12,45 @@
 
   ## Input check.
   assert_that(is.list(count_data) && has_attr(count_data, "names"))
-  data_type <- match.arg(str_to_lower(data_type), "tss")
   assert_that(is.flag(use_normalized))
 
   ## Change scores to normalized scores if requested.
   if (use_normalized) {
     walk(count_data, ~.x[, score := normalized_score])
+  }
+
+  ## If making a TSR matrix merge overlapping ranges.
+  if (data_type == "tsr") {
+
+    # Reduce overlapping ranges.
+    merged_ranges <- count_data %>%
+      map(as_granges) %>%
+      bind_ranges %>%
+      reduce_ranges_directed %>%
+      as.data.table
+
+    # Prepare reduced ranges for overlap with original ranges.
+    setkey(merged_ranges, seqnames, strand, start, end)
+    merged_ranges[,
+      FHASH := str_c(seqnames, start, end, strand),
+      by=seq_len(nrow(merged_ranges))
+    ][,
+      width := NULL
+    ]
+
+    # Get the aggregated score for reduced ranges.
+    count_data <- map(count_data, function(x) {
+      setkey(x, seqnames, strand, start, end)
+      x <- foverlaps(x, merged_ranges)
+      x[,
+        c("FHASH", "i.start", "i.end") := list(
+          i.FHASH, NULL, NULL
+        )
+      ]
+      x <- x[, .(score=sum(score)), by=FHASH]
+      return(x)
+    })
+    
   }
 
   ## Create the count matrix.
