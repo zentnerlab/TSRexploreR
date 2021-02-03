15adc142a803f7b48b3cab934972d35de18bacda
rpolicastro <rpolicas@indiana.edu>
2021-02-02 22:34:24 -0500

    adding yeast TSSs as a data package

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Yeast STRIPE-seq TSSs
+#'
+#' 3 Untreated and 3 Diamide treated yeast TSSs
+#' as determined by STRIPE-seq.
+#'
+#' @format A list of GRanges:
+#' @source \link{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE142524}
+"TSSs"
--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,13 +1,14 @@
-  library("magrittr")
-  library("purrr")
+library("TSRexploreR")
+library("magrittr")
+library("purrr")
 
-  ## Create example data.
-  TSSs <- readRDS(system.file("extdata", "S288C_TSSs.RDS", package="TSRexploreR"))
-  TSSs <- map(TSSs, function(x) {
-    x <- x[seqnames(x) == "I"]
-    return(x)
-  })
+## Create example data.
+TSSs <- readRDS(system.file("extdata", "S288C_TSSs.RDS", package="TSRexploreR"))
+TSSs <- map(TSSs, function(x) {
+  x <- x[seqnames(x) == "I"]
+  return(x)
+})
 
-  ## Genome assembly an annotation files.
-  assembly <- system.file("extdata", "S288C_Assembly.fasta", package="TSRexploreR")
-  annotation <- system.file("extdata", "S288C_Annotation.gtf", package="TSRexploreR")
+## Genome assembly an annotation files.
+assembly <- system.file("extdata", "S288C_Assembly.fasta", package="TSRexploreR")
+annotation <- system.file("extdata", "S288C_Annotation.gtf", package="TSRexploreR")
